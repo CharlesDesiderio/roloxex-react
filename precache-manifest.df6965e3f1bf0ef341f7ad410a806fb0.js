6116dae098262f2a15763e4722af0c43028f62c2
Charles Desiderio <cjdesi@gmail.com>
2020-05-27 14:16:30 -0500

    Updates

---
+++ precache-manifest.df6965e3f1bf0ef341f7ad410a806fb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f4c0a9a34f916f31c4b7e260abe3eb",
+    "url": "/roloxex-react/index.html"
+  },
+  {
+    "revision": "256f8aa9d6ae1b02e352",
+    "url": "/roloxex-react/static/css/main.1a2d96cd.chunk.css"
+  },
+  {
+    "revision": "dfe8eaee34b50b5a43cf",
+    "url": "/roloxex-react/static/js/2.0dae382a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roloxex-react/static/js/2.0dae382a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "256f8aa9d6ae1b02e352",
+    "url": "/roloxex-react/static/js/main.25f28cb2.chunk.js"
+  },
+  {
+    "revision": "5c177f231f1c6ac484ae",
+    "url": "/roloxex-react/static/js/runtime-main.e758f59e.js"
+  }
+]);
